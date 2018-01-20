52f0df1933fdbfe0f07999e8a601bb933ea72147
Benoit Lemaire <lemair_b@epita.fr>
2018-01-20 21:42:09 +0100

    Data updater : update tweets posted 3 days ago

--- src/update_data.py
+++ src/update_data.py
@@ -10,40 +10,43 @@ from elasticsearch import Elasticsearch
 
 import tweepy
 import time
+from time import gmtime
+
+import datetime
 
 def main():
-    for i in range(14, 20):
-        index = '2018.01.' + str(i)
+    delta_time = datetime.datetime.now() - datetime.timedelta(days=3)
+    index = delta_time.strftime("%Y.%m.%d")
     
-        try:
-            auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-            auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-            api = tweepy.API(auth)
-    
-            es = Elasticsearch()
-            res = es.search(index=index, doc_type='tweet', size=1000)
-            
-            tweets = res['hits']['hits']
+    try:
+        auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+        api = tweepy.API(auth)
         
-            for tweet in tweets:
-                try:
-                    source = tweet['_source']
-                    if not source['retweeted']:
-                        id_of_tweet = source['id_str']
-                        tweet = api.get_status(id_of_tweet)
-                        json_data = tweet._json
-                        es.index(index='tweet_updated_' + index, doc_type='tweet', body=json_data)
-                        print(id_of_tweet)
-                except tweepy.RateLimitError as e:
-                    print(e)
-                    time.sleep(60 * 15)
-                except tweepy.TweepError as e:
-                    print(e)
-                except Exception as e:
-                    print(e)
-                    
-        except Exception as e:
-            print(e)
+        es = Elasticsearch()
+        res = es.search(index=index, doc_type='tweet', size=1000)
+            
+        tweets = res['hits']['hits']
+        
+        for tweet in tweets:
+            try:
+                source = tweet['_source']
+                if not source['retweeted']:
+                    id_of_tweet = source['id_str']
+                    tweet = api.get_status(id_of_tweet)
+                    json_data = tweet._json
+                    es.index(index='tweet_updated_' + index, doc_type='tweet', body=json_data)
+                    print(id_of_tweet)
+            except tweepy.RateLimitError as e:
+                print(e)
+                time.sleep(60 * 15)
+            except tweepy.TweepError as e:
+                print(e)
+            except Exception as e:
+                print(e)
+                
+    except Exception as e:
+        print(e)
             
 if __name__ == "__main__":
     try:
